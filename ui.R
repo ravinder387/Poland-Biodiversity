e1ba9083872634e5710b20de7c1b5d4e93918998
ravinder ram <ravinderram29121993@gmail.com>
2023-12-13 16:17:00 +0530

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,53 @@
+# activate shiny pkg
+library(shiny)
+library(plotly)
+library(tidyverse)
+
+df <- read_csv("final.csv")
+
+mychoice <- vector("list", 689)
+names(mychoice) <- unique(df$scientificName)
+for(i in 1:length(mychoice)){
+  mychoice[[i]] = unique(df$scientificName)[i]
+}
+
+# build user interface
+ui <- fluidPage(
+  tags$head(tags$link(
+    rel = "stylesheet",
+    type = "text/css",
+    href = "style.css"
+  )),
+  fluidRow(
+    div(h2("Observation of Different Animal Species in Poland"),
+        
+        style = "
+        margin: 20px;
+        padding: 20px;
+        border-radius: 30px;
+background: #e0e0e0;
+box-shadow:  20px 20px 60px #bebebe,
+             -20px -20px 60px #ffffff;
+        ")
+  ),
+  fluidRow(
+    column(4, div(selectInput("bioinput", "Select Species:",mychoice),
+                  imageOutput("myImage"),
+                  style = "
+        margin: 20px;
+        padding: 20px;
+        border-radius: 30px;
+background: #e0e0e0;
+box-shadow:  20px 20px 60px #bebebe,
+             -20px -20px 60px #ffffff;")),
+    column(8, div( plotlyOutput("mymap"),
+                  style = "
+        margin: 20px;
+        padding: 5px;
+        border-radius: 30px;
+background: #e0e0e0;
+box-shadow:  20px 20px 60px #bebebe,
+             -20px -20px 60px #ffffff;
+        " ))
+  )
+)
